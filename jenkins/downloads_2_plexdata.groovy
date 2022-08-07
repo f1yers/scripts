d4c16937844fff75b24a1542c2188969ddbd7ccb
Nathan McGinnis <mcginnis.nathan@gmail.com>
2022-08-07 08:17:12 -0400

    error handling and logging

--- jenkins/downloads_2_plexdata.groovy
+++ jenkins/downloads_2_plexdata.groovy
@@ -20,21 +20,26 @@ pipeline {
                     for (s in seed_map) {
                         seed_list.add(s.name)
                     }
-                    completed = []
-                    println "Beginning mover loop..."
+                    in_progress = []
+                    println("Beginning mover loop...")
                     for (location in src_locations) {
+                        completed = []
                         def files = sh(script: "ls ${location}", returnStdout: true).trim()
                         def all_files = each_line(files, location)
                         for (file in all_files) {
                             if (!seed_list.contains(file)) {
                                 completed.add(file)
+                            } else {
+                                in_progress.add(file)
                             }
                         }
                         dest_location = '/plexdata/' + location.minus('/downloads/completed/')
+                        println("Will move the following files to ${dest_location}\r${completed}")
                         for (c in completed) {
                             mover(c, location, dest_location)
                         }
                     }
+                    println("Mover loop completed.  The following files were skipped because they are seeding:\r${in_progress}")
                 }
             }
         }
@@ -63,5 +68,9 @@ def seeding() {
 }
 
 def mover(file, src_location, dest_location) {
-    sh(script: """mv -fvn \"${src_location}/${file}\" ${dest_location}/""", returnStdout: true)
+    try {
+        sh(script: """mv -fvn \"${src_location}/${file}\" ${dest_location}/""", returnStdout: true)
+    } catch(Exception e) {
+        println("Failed to move \"${src_location}/${file}\" due to: ${e}")
+    }
 }
